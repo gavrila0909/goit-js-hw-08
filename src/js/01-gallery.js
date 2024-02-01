f2f53d4d7f6c1ab95f599c75d0836f885519d291
gavrila0909 <gavriladanna@gmail.com>
2024-02-01 17:55:23 +0000

    done task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,22 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryContainer = document.querySelector(".gallery");
+const createGalleryItems = galleryItems 
+    .map(({original, preview, description}) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}" />
+    </a>
+    </li>`
+    ).join("");
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryItems);
+const galleryHandler = new SimpleLightbox(".gallery a");
+galleryHandler.on("show.simpleLightbox");
+
+new SimpleLightbox('ul.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const key = 'feedback-form-state';
+
+const saveFormToLocalStorage = () => {
+  const formInputs = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(key, JSON.stringify(formInputs));
+};
+
+form.addEventListener('input', throttle(saveFormToLocalStorage, 500));
+
+window.addEventListener('load', () => {
+  const savedData = localStorage.getItem(key);
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formInputs = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.removeItem(key);
+  console.log('Form Inputs:', formInputs);
+  form.reset();
+});
